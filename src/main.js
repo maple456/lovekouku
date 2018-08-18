0ed2f578f89bbb7b7e61133f4eb36d324bbbea77
maple456 <42394631+maple456@users.noreply.github.com>
2018-08-19 07:58:36 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -126,7 +126,7 @@ class LINE extends Command {
         let receiver = messages.to;
         let sender = messages.from;
         
-        this.command('Halo', ['halo juga','ini siapa?']);
+        this.command('Halo', ['哈囉 juga','ini siapa?']);
         this.command('kamu siapa', this.getProfile.bind(this));
         this.command('.status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
         this.command(`.left ${payload}`, this.leftGroupByName.bind(this));
