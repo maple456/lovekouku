3e2b2b3a4f3468a343cc042b8a7e007044a4ba58
maple456 <42394631+maple456@users.noreply.github.com>
2018-08-20 02:33:37 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -126,7 +126,7 @@ class LINE extends Command {
         let receiver = messages.to;
         let sender = messages.from;
         
-        this.command('Halo', ['halo juga','ini siapa?']);
+        this.command('hello', ['hello','Who r u?']);
         this.command('kamu siapa', this.getProfile.bind(this));
         this.command('.status', `目前狀態: ${JSON.stringify(this.stateStatus)}`);
         this.command(`.left ${payload}`, this.leftGroupByName.bind(this));
